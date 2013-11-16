f9f4d93b942c27bb500460813858fcd0538c9f55
Yoichi Hirai <i@yoichihirai.com>
2013-11-16 21:53:31 +0900

    adding Kissdb struct

--- kissdb-rust.rs
+++ kissdb-rust.rs
@@ -1,5 +1,13 @@
 static version: uint = 1u;
 
+struct Kissdb {
+  hash_table_size : u8,
+  key_size : u8,
+  value_size : u8,
+  hash_table_size_bytes : u8,
+  hash_tables : ~u8,
+  f : std::rt::io::file::FileWriter
+}
 
 fn main()
 {
