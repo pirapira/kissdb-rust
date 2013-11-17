83844d99e8233491a89a94e4ccadd331c90fbaa4
Yoichi Hirai <i@yoichihirai.com>
2013-11-17 22:39:20 +0900

    hash funciton added

--- kissdb-rust.rs
+++ kissdb-rust.rs
@@ -4,11 +4,11 @@ static version: uint = 1u;
 // some definitions from kissdb.h
 
 struct Kissdb {
-  hash_table_size : u8,
-  key_size : u8,
-  value_size : u8,
-  hash_table_size_bytes : u8,
-  hash_tables : ~u8,
+  hash_table_size : u64,
+  key_size : u64,
+  value_size : u64,
+  hash_table_size_bytes : u64,
+  hash_tables : ~u64,
   f : std::rt::io::file::FileWriter
 }
 
@@ -25,6 +25,17 @@ enum OpenMode {
   RWCreate,
   RWReplace
 }
+
+
+fn kissdb_hash(b :&[u8], len: u64) -> u64
+{
+    let mut hash : u64 = 5381;
+    for i in range (0, len) {
+        hash = ((hash << 5) + hash) + (b[i] as u64);
+    }
+    return hash
+}
+
 
 fn main()
 {
